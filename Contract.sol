b376ce85115b7d8bafdbd8d55c909912ba75c899
Ankush Singh <ankush4singh@gmail.com>
2023-08-31 00:16:45 +0530

    Update Contract.sol

--- Contract.sol
+++ Contract.sol
@@ -3,20 +3,23 @@ pragma solidity 0.8.18;
 
 contract ExceptionExample {
     uint public value;
+    bool public valid = false;
 
     function setValue(uint _value) public {
         require(_value > 0, "Value must be greater than zero");
-
         value = _value;
+        valid = false;
     }
 
-    function assertExample(uint x, uint y) public pure {
+    function assertEqual(uint x, uint y) public {
         assert(x == y);
+        valid = true;
     }
 
-    function revertExample(uint x, uint y) public pure {
+    function isEqual(uint x, uint y) public {
         if (x != y) {
             revert("x and y must be equal");
         }
+        valid = true;
     }
 }
